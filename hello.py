8c887b8566d8d90f3c43291d7d3ecf8d542b1362
dhanushDM99 <21cs01071@iitbbs.ac.in>
2024-01-30 18:49:59 +0530

    Add Gradient Workflow

--- hello.py
+++ hello.py
@@ -1,21 +1,30 @@
-# #save this file as hello.py in your repo
-# import tensorflow as tf
-
-# # Simple hello world using TensorFlow
-# hello = tf.constant('Hello, TensorFlow!')
-
-# # Start tf session
-# sess = tf.Session()
-
-# # Run the op
-# print(sess.run(hello))
+print("Hello, World!")
 
 
 
-# Save this file as hello.py in your repository
 
-def main():
-    print("Hello, Paperspace Workflows!")
 
-if __name__ == "__main__":
-    main()
+
+
+
+# # #save this file as hello.py in your repo
+# # import tensorflow as tf
+
+# # # Simple hello world using TensorFlow
+# # hello = tf.constant('Hello, TensorFlow!')
+
+# # # Start tf session
+# # sess = tf.Session()
+
+# # # Run the op
+# # print(sess.run(hello))
+
+
+
+# # Save this file as hello.py in your repository
+
+# def main():
+#     print("Hello, Paperspace Workflows!")
+
+# if __name__ == "__main__":
+#     main()
